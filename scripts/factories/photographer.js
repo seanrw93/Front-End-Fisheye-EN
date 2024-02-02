8d04c231131406b899e6c0c0228a5077949c742b
seanrw93 <seanrw@gmail.com>
2024-02-02 15:09:04 +0100

    Correction picture link photographerFactory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
     const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/photographers_ID_Photos/${portrait}`;
+    const picture = `assets/photographers/Photographers_ID_Photos/${portrait}`;
 
    //Create img element
     function getImg() {
