42a27c56050e11b2d2f4aa9b56d969e12a1871e2
seanrw93 <seanrw@gmail.com>
2024-02-02 09:51:21 +0100

    Add photographers' photos

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -97,3 +97,32 @@ function photographerFactory(data) {
     //Return object with properties
     return { name, picture, city, country, tagline, price, getUserCardDOM, getPhotographerPageDOM }
 }
+
+function mediaFactory(data) {
+    const { image, video } = data;
+
+    function getMediaDOM() {
+        const mediaDiv = document.createElement( 'div' );
+        div.classList.add("media");
+
+        if (image) {
+            const img = document.createElement( 'img' );
+            img.setAttribute("src", `assets/photographers/photographers_ID_Photos/${image}`);
+            img.setAttribute("alt", "Image");
+            img.classList.add("media-image");
+            div.appendChild(img);
+        }
+
+        if (video) {
+            const video = document.createElement( 'video' );
+            video.setAttribute("src", `assets/photographers/photographers_ID_Photos/${video}`);
+            video.setAttribute("controls", true);
+            video.classList.add("media-video");
+            div.appendChild(video);
+        }
+
+        return mediaDiv;
+    }
+
+    return { image, video, getMediaDOM }
+}
