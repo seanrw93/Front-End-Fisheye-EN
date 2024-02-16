2c100f58b36f9a8b39a9d0a433c3b684f0943221
seanrw93 <seanrw@gmail.com>
2024-02-16 16:30:30 +0100

    Add total likes counter feature + update function names

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,7 +4,7 @@ function photographerFactory(data) {
     const picture = `assets/photographers/Photographers_ID_Photos/${portrait}`;
 
    //Create img element
-    function getImg() {
+    function createImg() {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         img.setAttribute("alt", name)
@@ -14,7 +14,7 @@ function photographerFactory(data) {
     }
 
     //Create name element
-    function getName() {
+    function createName() {
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         h2.classList.add("photographer-name")
@@ -22,7 +22,7 @@ function photographerFactory(data) {
     }
 
     //Create location element
-    function getLocation() {
+    function createLocation() {
         const h3 = document.createElement( 'h3' );
         h3.textContent = `${city}, ${country}`;
         h3.classList.add("photographer-location")
@@ -30,7 +30,7 @@ function photographerFactory(data) {
     }
 
     //Create tagline element
-    function getTagline() {
+    function createTagline() {
         const pTagline = document.createElement( 'p' );
         pTagline.textContent = tagline;
         pTagline.classList.add("photographer-tagline")
@@ -38,11 +38,73 @@ function photographerFactory(data) {
     }
 
     //Create price element
-    function getPrice() {   
+    function createPrice() {   
         const pPrice = document.createElement( 'p' );
         pPrice.textContent = `$${price}/day`;
         pPrice.classList.add("photographer-price")
         return pPrice;
+    }
+
+    //Create fixed counter element
+    function createFixedCounter() {
+
+        const likesCounter = document.querySelectorAll('.likes-counter');
+        const likesButton = document.querySelectorAll('.likes-button');
+
+        function updateTotalLikes() {
+            const totalLikes = Array.from(document.querySelectorAll('.likes-counter')).reduce((acc, like) => acc + parseInt(like.textContent), 0)
+
+            let totalCounter = Number(totalLikes)
+
+            likesButton.forEach(button => {
+                button.addEventListener('click', () => {
+                    likesCounter.forEach(like => {
+                        if (like.classList.contains('liked')) {
+                            totalCounter++;
+                            
+                        } else {
+                            totalCounter--;      
+                        }
+                    });
+                });
+            });
+
+            return totalCounter;
+        }
+
+        const container = document.createElement('div');
+        container.classList.add('fixed-counter');
+
+        const counterDiv = document.createElement('div');
+        counterDiv.classList.add('total-counter');
+
+        const counter = document.createElement('span');
+
+        //Initial total likes
+        counter.textContent = updateTotalLikes();
+        
+        //Update total likes when a like button is clicked
+        likesButton.forEach(button => {
+            button.addEventListener('click', () => {
+                counter.textContent = updateTotalLikes();
+            });
+        });
+        counterDiv.appendChild(counter);
+
+        const i = document.createElement('i');
+        i.classList.add('fas', 'fa-heart');
+        counterDiv.appendChild(i);
+
+        const priceDiv = document.createElement('div');
+
+        const pricePerDay = document.createElement('span');
+        pricePerDay.textContent = `$${price}/day`;
+        priceDiv.appendChild(pricePerDay);
+
+        container.appendChild(counterDiv);
+        container.appendChild(priceDiv);
+
+        return container;
     }
 
     function getUserCardDOM() {
@@ -54,15 +116,15 @@ function photographerFactory(data) {
         const link = document.createElement( 'a' );
         link.setAttribute("href", `./photographer.html?id=${data.id}`)
 
-        const img = getImg();
+        const img = createImg();
 
         //Append img to link
         link.appendChild(img)
 
-        const h2 = getName();
-        const h3 = getLocation();
-        const pTagline = getTagline();
-        const pPrice = getPrice();
+        const h2 = createName();
+        const h3 = createLocation();
+        const pTagline = createTagline();
+        const pPrice = createPrice();
 
         //Append elements to article
         article.appendChild(link);
@@ -75,11 +137,13 @@ function photographerFactory(data) {
     }
 
     function getPhotographerPageDOM() {
+
+        // Create picture info elements
         const divInfo = document.querySelector(".photographer-info");
 
-        const h2 = getName();
-        const h3 = getLocation();
-        const pTagline = getTagline();
+        const h2 = createName();
+        const h3 = createLocation();
+        const pTagline = createTagline();
 
         divInfo.appendChild(h2);
         divInfo.appendChild(h3);
@@ -87,11 +151,16 @@ function photographerFactory(data) {
 
         const divPicture = document.querySelector(".photographer-picture");
 
-        const img = getImg();
+        const img = createImg();
         divPicture.appendChild(img);
 
-        return { divInfo, divPicture };
+        //Create fixed counter
+        const body = document.querySelector('body');
 
+        const divCounter = createFixedCounter();
+        body.appendChild(divCounter);
+
+        return { divInfo, divPicture, body };
     }
 
     //Return object with properties
